ed85e4cccd0e7819935a33d5007dcea33150b91e
Gentuar Lushtaku <gl55390@ubt-uni.net>
2023-05-11 22:26:39 -0700

    Added Login Logic

--- OnlineJobPortal/User/Register.aspx.cs
+++ OnlineJobPortal/User/Register.aspx.cs
@@ -16,6 +16,25 @@ namespace OnlineJobPortal.User
         string str = ConfigurationManager.ConnectionStrings["cs"].ConnectionString;
         protected void Page_Load(object sender, EventArgs e)
         {
+
+        }
+
+        private void clear()
+        {
+            txtUserName.Text = string.Empty;
+            txtAdress.Text = string.Empty;
+            txtEmail.Text = string.Empty;
+            txtMobile.Text = string.Empty;
+            txtUserName.Text = string.Empty;
+            txtFullName.Text = string.Empty;
+            txtPassword.Text = string.Empty;
+            txtConfirmPassword.Text = string.Empty;
+            ddlCountry.ClearSelection();
+
+        }
+
+        protected void btnRegister_Click(object sender, EventArgs e)
+        {
             try
             {
                 con = new SqlConnection(str);
@@ -24,7 +43,7 @@ namespace OnlineJobPortal.User
                 cmd.Parameters.AddWithValue("@Username", txtUserName.Text.Trim());
                 cmd.Parameters.AddWithValue("@Password", txtConfirmPassword.Text.Trim());
                 cmd.Parameters.AddWithValue("@Name", txtFullName.Text.Trim());
-                cmd.Parameters.AddWithValue("@Adress",txtAdress.Text.Trim());
+                cmd.Parameters.AddWithValue("@Adress", txtAdress.Text.Trim());
                 cmd.Parameters.AddWithValue("@Mobile", txtMobile.Text.Trim());
                 cmd.Parameters.AddWithValue("@Email", txtEmail.Text.Trim());
                 cmd.Parameters.AddWithValue("@Country", ddlCountry.SelectedValue);
@@ -34,7 +53,7 @@ namespace OnlineJobPortal.User
                 if (r > 0)
                 {
                     lblMsg.Visible = true;
-                    lblMsg.Text = "Registered Succsesfuly";
+                    lblMsg.Text = "Registered Successfully";
                     lblMsg.CssClass = "alert alert-success";
                     clear();
                 }
@@ -52,7 +71,7 @@ namespace OnlineJobPortal.User
                 if (ex.Message.Contains("Violation of UNIQUE KEY constraint"))
                 {
                     lblMsg.Visible = true;
-                    lblMsg.Text = "<b>"+txtUserName.Text.Trim()+"</b>"+"Username already exist ,try new one";
+                    lblMsg.Text = "<b>" + txtUserName.Text.Trim() + "</b>" + "Username already exists, please try a new one.";
                     lblMsg.CssClass = "alert alert-danger";
                 }
                 else
@@ -71,41 +90,5 @@ namespace OnlineJobPortal.User
             }
         }
 
-        private void clear()
-        {
-            txtUserName.Text = string.Empty;
-            txtAdress.Text = string.Empty;
-            txtEmail.Text = string.Empty;
-            txtMobile.Text = string.Empty;
-            txtUserName.Text = string.Empty;
-            txtFullName.Text = string.Empty;
-            txtPassword.Text = string.Empty;
-            txtConfirmPassword.Text = string.Empty;
-            ddlCountry.ClearSelection();
-
-
-
-
-        }
-
-        //protected void ddlCountry_SelectedIndexChanged(object sender, EventArgs e)
-        //{
-
-        //}
-
-        //protected void Unnamed3_Selecting(object sender, SqlDataSourceSelectingEventArgs e)
-        //{
-
-        //}
-
-        protected void btnRegister_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        protected void SqlDataSource1_Selecting(object sender, SqlDataSourceSelectingEventArgs e)
-        {
-
-        }
     }
 }
